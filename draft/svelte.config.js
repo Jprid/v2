94b4239b4e8ffcbe9b575d65f57d86c608b186b3
Jprid <mackattack.bg@gmail.com>
2025-01-13 15:48:37 -0600

    fix up configs

--- draft/svelte.config.js
+++ draft/svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({pages: 'build', assets: 'build', strict: false}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '' : '',
+			base: process.env.NODE_ENV === 'production' ? '/v2' : '',
 		}
 	}
 };
