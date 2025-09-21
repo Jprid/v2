b6c7a306f1e2efe1a334b7bbfc748781012bbe01
unset <mackattack.bg@gmail.com>
2025-09-21 18:30:12 -0500

    Updates

--- _app/immutable/chunks/C_ZeE_W3.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_176d5l?.base??"/v2",a=globalThis.__sveltekit_176d5l?.assets??s;export{a,s as b};
---
+++ _app/immutable/chunks/gNPw_Wcj.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_tnifvb?.base??"/v2",t=globalThis.__sveltekit_tnifvb?.assets??s;export{t as a,s as b};
